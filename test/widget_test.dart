23e0594a01a10229e410c529a607f125256dd276
joseechf <josechdev@gmail.com>
2026-03-03 04:57:51 -0500

    test con startLocale

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,19 +6,23 @@ import 'package:ecoazuero/main.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
+  setUpAll(() async {
+    await EasyLocalization.ensureInitialized();
+  });
+
   testWidgets('App loads correctly', (WidgetTester tester) async {
     await tester.pumpWidget(
       EasyLocalization(
         supportedLocales: const [Locale('en'), Locale('es')],
         path: 'assets/translations',
         fallbackLocale: const Locale('es'),
+        startLocale: const Locale('es'),
         child: const AppLoader(),
       ),
     );
 
     await tester.pumpAndSettle();
 
-    // verificar que MaterialApp exista
     expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
